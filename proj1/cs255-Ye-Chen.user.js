24a4a3e71a7d05be97905de0b6a6af49a3486a74
Frank Chen <frankchn@gmail.com>
2013-01-20 23:02:16 -0800

    core AES encryption and decryption functions

--- proj1/cs255-Ye-Chen.user.js
+++ proj1/cs255-Ye-Chen.user.js
@@ -74,8 +74,9 @@ function Decrypt(cipherText, group) {
 // @param {String} group Group name.
 function GenerateKey(group) {
 
-  // CS255-todo: Well this needs some work...
-  var key = 'CS255-todo';
+  // this generates a random 128-bit key using crypto primitives
+  var key = new Uint32Array(4);
+  window.crypto.getRandomValues(key);
 
   keys[group] = key;
   SaveKeys();
@@ -92,14 +93,265 @@ function SaveKeys() {
 
 // Load the group keys from disk.
 function LoadKeys() {
+  var master_passphrase = prompt("Please enter your master key for encryption and decryption");
+  var master_key = KeyDerivation(master_passphrase);
+
+  SetMasterKey(master_key);
+
   keys = {}; // Reset the keys.
   var saved = localStorage.getItem('facebook-keys-' + my_username);
   if (saved) {
     var key_str = decodeURIComponent(saved);
-    // CS255-todo: plaintext keys were on disk?
-    keys = JSON.parse(key_str);
+  
+    // TODO: VERIFY MAC IN PART II OF THE MILESTONE!!  
+    var cipher = new sjcl.cipher.aes(master_key);
+    plain_str = cipher.decrypt(key_str);
+
+    keys = JSON.parse(plain_str);
   }
 }
+
+function AES_Extract_Block(str) {
+  var data = new Array(4);
+
+  for(var i = 0; i < 4; i++) {
+    data[i] = 0;
+    for(var j = 0; j < 4; j++) {
+      var pos = i * 4 + j;
+      data[i] += str.charCodeAt(pos) << (j * 8);
+    }
+  }
+
+  return data;
+}
+
+function AES_Reconstruct_String(block) {
+  var str = "";
+  var data = Array.prototype.slice.call(block);
+
+  for(var i = 0; i < 4; i++) {
+    for(var j = 0; j < 4; j++) {
+      var tmp = data[i] & 0xFF;
+      str += String.fromCharCode(tmp);
+      data[i] = data[i] >> 8;
+    }
+  }
+
+  return str;
+}
+
+// Implements AES-CTR-128
+function AES_Encrypt_String(key, plaintext) {
+  var nonce = new Uint32Array(4);
+  window.crypto.getRandomValues(nonce);
+
+  var cipher = new sjcl.cipher.aes(key);
+  var ciphertext = AES_Reconstruct_String(nonce);
+
+  var num_blocks = Math.ceil(plaintext.length / 16);
+
+  for(var i = 0; i < num_blocks; i++) {
+    var data = Array.prototype.slice.call(nonce);
+    data[3] ^= i;
+
+    var stream = cipher.encrypt(data);
+    var pt_block = AES_Extract_Block(plaintext.substring(i * 16, (i + 1) * 16));
+
+    for(var j = 0; j < 4; j++) {
+      stream[j] ^= pt_block[j];
+    }
+
+    ciphertext += AES_Reconstruct_String(stream);
+  }
+
+  return ciphertext;
+}
+
+function AES_Decrypt_String(key, nonceciphertext) {
+  var nonce = AES_Extract_Block(nonceciphertext.substring(0, 16));
+  var cipher = new sjcl.cipher.aes(key);
+  var plaintext = "";
+
+  var ciphertext = nonceciphertext.substr(16);
+  var num_blocks = Math.ceil(ciphertext.length / 16);
+
+  for(var i = 0; i < num_blocks; i++) {
+    var data = Array.prototype.slice.call(nonce);
+    data[3] ^= i;
+
+    var stream = cipher.encrypt(data);
+    var pt_block = AES_Extract_Block(ciphertext.substring(i * 16, (i + 1) * 16));
+
+    for(var j = 0; j < 4; j++) {
+      stream[j] ^= pt_block[j];
+    }
+
+    plaintext += AES_Reconstruct_String(stream);
+  }
+
+  return plaintext;
+}
+
+function SetMasterKey(k) {
+  createSessionCookie("master-key-0", k[0]);
+  createSessionCookie("master-key-1", k[1]);
+  createSessionCookie("master-key-2", k[2]);
+  createSessionCookie("master-key-3", k[3]);
+}
+
+function GetMasterKey() {
+  var master_key = new Array(4);
+  master_key[0] = readCookie("master-key-0");
+  master_key[1] = readCookie("master-key-1");
+  master_key[2] = readCookie("master-key-2");
+  master_key[3] = readCookie("master-key-3");
+  return master_key;
+}
+
+function ClearMasterKey() {
+  eraseCookie("master-key-0");
+  eraseCookie("master-key-1");
+  eraseCookie("master-key-2");
+  eraseCookie("master-key-3"); 
+}
+
+// Implementation of PBKDF1 with SHA1
+function KeyDerivation(password) {
+  var salt = String.fromCharCode(43, 99, 8, 99, 32, 47, 39, 40, 
+                                 43, 121, 59, 205, 210, 171, 20, 5, 
+                                 129, 251, 25, 19, 201, 29, 212, 87, 
+                                 133, 236, 116, 228, 201, 202, 35, 171);
+  var iterations = 1000;
+  var dk = password + salt;
+  var key = new Array(4);
+
+  for(var i = 0; i < iterations; i++) {
+    var tmp = Sha1.hash(dk);
+    dk = '';
+    if(i < iterations - 1) {
+      for(var l = 0; l < tmp.length; l = l + 2) 
+        dk += String.fromCharCode(parseInt(tmp.substring(l, l + 2), 16));
+    } else {
+      key[0] = parseInt(tmp.substring(0, 8), 16);
+      key[1] = parseInt(tmp.substring(8, 16), 16);
+      key[2] = parseInt(tmp.substring(16, 24), 16);
+      key[3] = parseInt(tmp.substring(24, 32), 16);
+    }
+  }
+
+  return key;
+}
+
+function _TestFramework() {
+  var master_key = KeyDerivation("This is a test!");
+
+  var encrypted_string = AES_Encrypt_String(master_key, "asdgasdg");
+  var decrypted_string = AES_Decrypt_String(master_key, encrypted_string);
+
+  console.log("DECRYPTED STRING = " + decrypted_string);
+}
+
+
+/////////////////////////////////////////////////////////
+// SHA-1 Implementation in JavaScript
+// By Chris Veness - http://www.movable-type.co.uk/scripts/sha1.html
+/////////////////////////////////////////////////////////
+
+var Sha1 = {};
+
+Sha1.hash = function(msg) {  
+
+  var K = [0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6];  
+  msg += String.fromCharCode(0x80);  // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
+  
+  // convert string msg into 512-bit/16-integer blocks arrays of ints [§5.2.1]
+  var l = msg.length/4 + 2;  // length (in 32-bit integers) of msg + ‘1’ + appended length
+  var N = Math.ceil(l/16);   // number of 16-integer-blocks required to hold 'l' ints
+  var M = new Array(N);
+  
+  for (var i=0; i<N; i++) {
+    M[i] = new Array(16);
+    for (var j=0; j<16; j++) {  // encode 4 chars per integer, big-endian encoding
+      M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) | 
+        (msg.charCodeAt(i*64+j*4+2)<<8) | (msg.charCodeAt(i*64+j*4+3));
+    } // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
+  }
+
+  M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14])
+  M[N-1][15] = ((msg.length-1)*8) & 0xffffffff;
+  
+  // set initial hash value [§5.3.1]
+  var H0 = 0x67452301;
+  var H1 = 0xefcdab89;
+  var H2 = 0x98badcfe;
+  var H3 = 0x10325476;
+  var H4 = 0xc3d2e1f0;
+  
+  // HASH COMPUTATION [§6.1.2]
+  
+  var W = new Array(80); var a, b, c, d, e;
+  for (var i=0; i<N; i++) {
+  
+    // 1 - prepare message schedule 'W'
+    for (var t=0;  t<16; t++) W[t] = M[i][t];
+    for (var t=16; t<80; t++) W[t] = Sha1.ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1);
+    
+    // 2 - initialise five working variables a, b, c, d, e with previous hash value
+    a = H0; b = H1; c = H2; d = H3; e = H4;
+    
+    // 3 - main loop
+    for (var t=0; t<80; t++) {
+      var s = Math.floor(t/20); // seq for blocks of 'f' functions and 'K' constants
+      var T = (Sha1.ROTL(a,5) + Sha1.f(s,b,c,d) + e + K[s] + W[t]) & 0xffffffff;
+      e = d;
+      d = c;
+      c = Sha1.ROTL(b, 30);
+      b = a;
+      a = T;
+    }
+    
+    // 4 - compute the new intermediate hash value
+    H0 = (H0+a) & 0xffffffff;  // note 'addition modulo 2^32'
+    H1 = (H1+b) & 0xffffffff; 
+    H2 = (H2+c) & 0xffffffff; 
+    H3 = (H3+d) & 0xffffffff; 
+    H4 = (H4+e) & 0xffffffff;
+  }
+
+  return Sha1.toHexStr(H0) + Sha1.toHexStr(H1) + 
+    Sha1.toHexStr(H2) + Sha1.toHexStr(H3) + Sha1.toHexStr(H4);
+}
+
+//
+// function 'f' [§4.1.1]
+//
+Sha1.f = function(s, x, y, z)  {
+  switch (s) {
+  case 0: return (x & y) ^ (~x & z);           // Ch()
+  case 1: return x ^ y ^ z;                    // Parity()
+  case 2: return (x & y) ^ (x & z) ^ (y & z);  // Maj()
+  case 3: return x ^ y ^ z;                    // Parity()
+  }
+}
+
+//
+// rotate left (circular left shift) value x by n positions [§3.2.5]
+//
+Sha1.ROTL = function(x, n) {
+  return (x<<n) | (x>>>(32-n));
+}
+
+//
+// hexadecimal representation of a number 
+//   (note toString(16) is implementation-dependant, and  
+//   in IE returns signed numbers when used on full words)
+//
+Sha1.toHexStr = function(n) {
+  var s="", v;
+  for (var i=7; i>=0; i--) { v = (n>>>(i*4)) & 0xf; s += v.toString(16); }
+  return s;
+}
+
 
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
@@ -1277,7 +1529,7 @@ console.log("CS255 script finished loading.");
 // Stub for phantom.js (http://phantomjs.org/)
 if (typeof phantom !== "undefined") {
   console.log("Hello! You're running in phantom.js.");
-  // Add any function calls you want to run.
+  _TestFramework();
   phantom.exit();
 }
 
