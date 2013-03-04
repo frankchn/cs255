883cb76ed20fe0e59da465b65cb7795f1c77ea17
Frank Chen <frankchn@gmail.com>
2013-03-03 17:30:14 -0800

    fix indentation

--- proj2/mitm/MITMAdminServer.java
+++ proj2/mitm/MITMAdminServer.java
@@ -17,76 +17,76 @@ class MITMAdminServer implements Runnable
     private HTTPSProxyEngine m_engine;
     
     public MITMAdminServer( String localHost, int adminPort, HTTPSProxyEngine engine ) throws IOException,GeneralSecurityException {
-	MITMSSLSocketFactory socketFactory = new MITMSSLSocketFactory();
-				
-	m_serverSocket = socketFactory.createServerSocket( localHost, adminPort, 0 );
-	m_engine = engine;
+        MITMSSLSocketFactory socketFactory = new MITMSSLSocketFactory();
+                                
+        m_serverSocket = socketFactory.createServerSocket( localHost, adminPort, 0 );
+        m_engine = engine;
     }
 
     public void run() {
-	System.out.println("Admin server initialized, listening on port " + m_serverSocket.getLocalPort());
-	while( true ) {
-	    try {
-		m_socket = m_serverSocket.accept();
+        System.out.println("Admin server initialized, listening on port " + m_serverSocket.getLocalPort());
+        while( true ) {
+            try {
+                m_socket = m_serverSocket.accept();
 
-		byte[] buffer = new byte[40960];
+                byte[] buffer = new byte[40960];
 
-		Pattern userPwdPattern =
-		    Pattern.compile("password:(\\S+)\\s+command:(\\S+)\\sCN:(\\S*)\\s");
-		
-		BufferedInputStream in =
-		    new BufferedInputStream(m_socket.getInputStream(),
-					    buffer.length);
+                Pattern userPwdPattern =
+                    Pattern.compile("password:(\\S+)\\s+command:(\\S+)\\sCN:(\\S*)\\s");
+                
+                BufferedInputStream in =
+                    new BufferedInputStream(m_socket.getInputStream(),
+                                            buffer.length);
 
-		// Read a buffer full.
-		int bytesRead = in.read(buffer);
+                // Read a buffer full.
+                int bytesRead = in.read(buffer);
 
-		String line =
-		    bytesRead > 0 ?
-		    new String(buffer, 0, bytesRead) : "";
+                String line =
+                    bytesRead > 0 ?
+                    new String(buffer, 0, bytesRead) : "";
 
-		Matcher userPwdMatcher =
-		    userPwdPattern.matcher(line);
+                Matcher userPwdMatcher =
+                    userPwdPattern.matcher(line);
 
-		// parse username and pwd
-		if (userPwdMatcher.find()) {
-		    String password = userPwdMatcher.group(1);
+                // parse username and pwd
+                if (userPwdMatcher.find()) {
+                    String password = userPwdMatcher.group(1);
 
-		    // TODO(cs255): authenticate the user
+                    // TODO(cs255): authenticate the user
 
-		    boolean authenticated = true;
+                    boolean authenticated = true;
 
-		    // if authenticated, do the command
-		    if( authenticated ) {
-			String command = userPwdMatcher.group(2);
-			String commonName = userPwdMatcher.group(3);
+                    // if authenticated, do the command
+                    if( authenticated ) {
+                        String command = userPwdMatcher.group(2);
+                        String commonName = userPwdMatcher.group(3);
 
-			doCommand( command );
-		    }
-		}	
-	    }
-	    catch( InterruptedIOException e ) {
-	    }
-	    catch( Exception e ) {
-		e.printStackTrace();
-	    }
-	}
+                        doCommand( command );
+                    }
+                }       
+            }
+            catch( InterruptedIOException e ) {
+            }
+            catch( Exception e ) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void sendString(final String str) throws IOException {
-	PrintWriter writer = new PrintWriter( m_socket.getOutputStream() );
-	writer.println(str);
-	writer.flush();
+        PrintWriter writer = new PrintWriter( m_socket.getOutputStream() );
+        writer.println(str);
+        writer.flush();
     }
     
     private void doCommand( String cmd ) throws IOException {
 
-	// TODO(cs255): instead of greeting admin client, run the indicated command
+        // TODO(cs255): instead of greeting admin client, run the indicated command
 
-	sendString("How are you Admin Client !!");
+        sendString("How are you Admin Client !!");
 
-	m_socket.close();
-	
+        m_socket.close();
+        
     }
 
 }
--- proj2/mitm/MITMProxyServer.java
+++ proj2/mitm/MITMProxyServer.java
@@ -15,40 +15,40 @@ import java.io.PrintWriter;
 public class MITMProxyServer
 {
     public static void main(String[] args) {
-	final MITMProxyServer proxy = new MITMProxyServer(args);
-	proxy.run();
+        final MITMProxyServer proxy = new MITMProxyServer(args);
+        proxy.run();
     }
 
     private Error printUsage() {
-	System.err.println(
-	    "\n" +
-	    "Usage: " +
-	    "\n java " + MITMProxyServer.class + " <options>" +
-	    "\n" +
-	    "\n Where options can include:" +
-	    "\n" +
-	    "\n   [-localHost <host name/ip>]  Default is localhost" +
-	    "\n   [-localPort <port>]          Default is 8001" +
-	    "\n   [-adminPort <port>]          Default is 8002" +
-	    "\n   [-pwdFile <file> ]           Password file" +
-	    "\n   [-keyStore <file>]           Key store details for" +
-	    "\n   [-keyStorePassword <pass>]   certificates. Equivalent to" +
-	    "\n   [-keyStoreType <type>]       javax.net.ssl.XXX properties" +
-	    "\n   [-keyStoreAlias <alias>]     Default is keytool default of 'mykey'" +
-	    "\n   [-outputFile <filename>]     Default is stdout" +
-	    "\n" +
-	    "\n -outputFile specifies where the output from ProxyDataFilter will go." +
-	    "\n By default, it is sent to stdout" +
-	    "\n"
-	    );
+        System.err.println(
+            "\n" +
+            "Usage: " +
+            "\n java " + MITMProxyServer.class + " <options>" +
+            "\n" +
+            "\n Where options can include:" +
+            "\n" +
+            "\n   [-localHost <host name/ip>]  Default is localhost" +
+            "\n   [-localPort <port>]          Default is 8001" +
+            "\n   [-adminPort <port>]          Default is 8002" +
+            "\n   [-pwdFile <file> ]           Password file" +
+            "\n   [-keyStore <file>]           Key store details for" +
+            "\n   [-keyStorePassword <pass>]   certificates. Equivalent to" +
+            "\n   [-keyStoreType <type>]       javax.net.ssl.XXX properties" +
+            "\n   [-keyStoreAlias <alias>]     Default is keytool default of 'mykey'" +
+            "\n   [-outputFile <filename>]     Default is stdout" +
+            "\n" +
+            "\n -outputFile specifies where the output from ProxyDataFilter will go." +
+            "\n By default, it is sent to stdout" +
+            "\n"
+        );
 
-	System.exit(1);
-	return null;
+        System.exit(1);
+        return null;
     }
 
     private Error printUsage(String s) {
-	System.err.println("\n" + "Error: " + s);
-	throw printUsage();
+        System.err.println("\n" + "Error: " + s);
+        throw printUsage();
     }
 
     private HTTPSProxyEngine m_engine = null;
@@ -56,93 +56,93 @@ public class MITMProxyServer
     
     private MITMProxyServer(String[] args)
     {
-	// Default values.
-	ProxyDataFilter requestFilter = new ProxyDataFilter();
-	ProxyDataFilter responseFilter = new ProxyDataFilter();
-	int localPort = 8001;
-	int adminPort = 8002;
-	String localHost = "localhost";
+        // Default values.
+        ProxyDataFilter requestFilter = new ProxyDataFilter();
+        ProxyDataFilter responseFilter = new ProxyDataFilter();
+        int localPort = 8001;
+        int adminPort = 8002;
+        String localHost = "localhost";
 
-	int timeout = 0; 
-	String filename = null;
+        int timeout = 0; 
+        String filename = null;
 
-	try {
-	    for (int i=0; i<args.length; i++)
-	    {
-		if (args[i].equals("-localHost")) {
-		    localHost = args[++i];
-		} else if (args[i].equals("-localPort")) {
-		    localPort = Integer.parseInt(args[++i]);
-		} else if (args[i].equals("-keyStore")) {
-		    System.setProperty(JSSEConstants.KEYSTORE_PROPERTY,
-				       args[++i]);
-		} else if (args[i].equals("-keyStorePassword")) {
-		    System.setProperty(
-			JSSEConstants.KEYSTORE_PASSWORD_PROPERTY, args[++i]);
-		} else if (args[i].equals("-keyStoreType")) {
-		    System.setProperty(JSSEConstants.KEYSTORE_TYPE_PROPERTY,
-				       args[++i]);
-		} else if (args[i].equals("-keyStoreAlias")) {
-		    System.setProperty(JSSEConstants.KEYSTORE_ALIAS_PROPERTY,
-				       args[++i]);
-		} else if (args[i].equals("-timeout")) {
-		    timeout = Integer.parseInt(args[++i]) * 1000;
-		} else if( args[i].equals("-pwdFile")) {
-			i++; // TODO(cs255): parse this as needed
-		} else if (args[i].equals("-adminPort")) {
-			adminPort = Integer.parseInt(args[++i]);
-		} else if (args[i].equals("-outputFile")) {
-		    PrintWriter pw = new PrintWriter(new FileWriter(args[++i]), true);
-		    requestFilter.setOutputPrintWriter(pw);
-		    responseFilter.setOutputPrintWriter(pw);
-		} else {
-		    throw printUsage();
-		}
-	    }
-	}
-	catch (Exception e) {
-	    throw printUsage();
-	}
+        try {
+            for (int i=0; i<args.length; i++)
+            {
+                if (args[i].equals("-localHost")) {
+                    localHost = args[++i];
+                } else if (args[i].equals("-localPort")) {
+                    localPort = Integer.parseInt(args[++i]);
+                } else if (args[i].equals("-keyStore")) {
+                    System.setProperty(JSSEConstants.KEYSTORE_PROPERTY,
+                                       args[++i]);
+                } else if (args[i].equals("-keyStorePassword")) {
+                    System.setProperty(
+                        JSSEConstants.KEYSTORE_PASSWORD_PROPERTY, args[++i]);
+                } else if (args[i].equals("-keyStoreType")) {
+                    System.setProperty(JSSEConstants.KEYSTORE_TYPE_PROPERTY,
+                                       args[++i]);
+                } else if (args[i].equals("-keyStoreAlias")) {
+                    System.setProperty(JSSEConstants.KEYSTORE_ALIAS_PROPERTY,
+                                       args[++i]);
+                } else if (args[i].equals("-timeout")) {
+                    timeout = Integer.parseInt(args[++i]) * 1000;
+                } else if( args[i].equals("-pwdFile")) {
+                        i++; // TODO(cs255): parse this as needed
+                } else if (args[i].equals("-adminPort")) {
+                        adminPort = Integer.parseInt(args[++i]);
+                } else if (args[i].equals("-outputFile")) {
+                    PrintWriter pw = new PrintWriter(new FileWriter(args[++i]), true);
+                    requestFilter.setOutputPrintWriter(pw);
+                    responseFilter.setOutputPrintWriter(pw);
+                } else {
+                    throw printUsage();
+                }
+            }
+        }
+        catch (Exception e) {
+            throw printUsage();
+        }
 
-	if (timeout < 0) {
-	    throw printUsage("Timeout must be non-negative");
-	}
+        if (timeout < 0) {
+            throw printUsage("Timeout must be non-negative");
+        }
 
-	final StringBuffer startMessage = new StringBuffer();
+        final StringBuffer startMessage = new StringBuffer();
 
-	startMessage.append("Initializing SSL proxy with the parameters:" +
-	    "\n   Local host:       " + localHost + 
-	    "\n   Local port:       " + localPort +
-	    "\n   Admin port:       " + adminPort);
-	startMessage.append("\n   (SSL setup could take a few seconds)");
+        startMessage.append("Initializing SSL proxy with the parameters:" +
+            "\n   Local host:       " + localHost + 
+            "\n   Local port:       " + localPort +
+            "\n   Admin port:       " + adminPort);
+        startMessage.append("\n   (SSL setup could take a few seconds)");
 
-	System.err.println(startMessage);
+        System.err.println(startMessage);
 
-	try {
-	    m_engine = 
-		new HTTPSProxyEngine(new MITMPlainSocketFactory(),
-				     new MITMSSLSocketFactory(),
-				     requestFilter,
-				     responseFilter,
-				     localHost,
-				     localPort,
-				     timeout);
-	    m_adminServer = new MITMAdminServer( localHost, adminPort, m_engine );
-	    
-	    System.err.println("Proxy initialized, listening on port " + localPort);
-	}
-	catch (Exception e){
-	    System.err.println("Could not initialize proxy:");
-	    e.printStackTrace();
-	    System.exit(2);
-	}
+        try {
+            m_engine = 
+                new HTTPSProxyEngine(new MITMPlainSocketFactory(),
+                                     new MITMSSLSocketFactory(),
+                                     requestFilter,
+                                     responseFilter,
+                                     localHost,
+                                     localPort,
+                                     timeout);
+            m_adminServer = new MITMAdminServer( localHost, adminPort, m_engine );
+            
+            System.err.println("Proxy initialized, listening on port " + localPort);
+        }
+        catch (Exception e){
+            System.err.println("Could not initialize proxy:");
+            e.printStackTrace();
+            System.exit(2);
+        }
     }
 
     public void run() 
     {
-	(new Thread(m_adminServer)).start();
-	m_engine.run();
-	System.err.println("Engine exited");
-	System.exit(0);
+        (new Thread(m_adminServer)).start();
+        m_engine.run();
+        System.err.println("Engine exited");
+        System.exit(0);
     }
 }
