fc21577a95cb03b8d1094d0872418654e46e6998
Frank Chen <frankchn@gmail.com>
2013-03-11 23:11:32 -0700

    hmm nvm

--- proj2/run.sh
+++ proj2/run.sh
@@ -2,4 +2,4 @@
 make clean
 make
 export CLASSPATH="${CLASSPATH}:.:iaik_jce.jar"
-java mitm.MITMProxyServer -keyStore keystore -keyStorePassword cs255test -pwdFile pwdfile -keyStoreAlias mykey
+java mitm.MITMProxyServer -keyStore keystore -keyStorePassword cs255test -pwdFile pwdfile -keyStoreAlias mykey 
